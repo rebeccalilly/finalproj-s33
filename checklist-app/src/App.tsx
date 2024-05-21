09accb6fb84703373ac1a26a567763f53f39a8ac
Rebecca Anderson <randers2@bates.edu>
2024-05-21 10:25:34 -0400

    changed App.tsx

--- checklist-app/src/App.tsx
+++ checklist-app/src/App.tsx
@@ -1,8 +1,11 @@
-import './style.css'
-import Checklist from './Checklist'
-import Profile from './Profile'
+import { useState } from 'react';
+import './style.css';
+import Checklist from './Checklist';
+import Profile from './Profile';
+import Authentication from './authentication';
 
 function App() {
+  const [whichPage, setWhichPage] = useState(0);
 
   return (
     <div>
