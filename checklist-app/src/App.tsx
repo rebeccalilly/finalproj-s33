e63e1546c7efc451cd1e110100864d3f004aa735
Rebecca Anderson <randers2@bates.edu>
2024-05-21 09:45:49 -0400

    added Checklist, Listitem, and very minimal Profile components

--- checklist-app/src/App.tsx
+++ checklist-app/src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './style.css'
+import Checklist from './Checklist'
+import Profile from './Profile'
 
 function App() {
 
   return (
-    <div>Home</div>
+    <div>
+      <Checklist />
+    </div>
   )
 }
 
