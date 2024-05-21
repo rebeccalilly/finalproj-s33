1d8679f9429fef33a36eb7abdda6b1ea5e760872
Rebecca Anderson <randers2@bates.edu>
2024-05-21 10:41:07 -0400

    adding imports to App.tsx

--- checklist-app/src/App.tsx
+++ checklist-app/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import './style.css';
-import Checklist from './Checklist';
+import Checklist from './checklist';
 import Profile from './Profile';
 import Authentication from './authentication';
+import Checklistcard from './Checklistcard';
 
 function App() {
   const [whichPage, setWhichPage] = useState(0);
