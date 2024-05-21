462e809bd970e02dc0829978a76d6194bef94cc0
Jelizaveta Tsalaja <jelizaveta.tsalaja@gmail.com>
2024-05-21 12:00:16 -0400

    updated log in sign up

--- checklist-app/src/authentication.tsx
+++ checklist-app/src/authentication.tsx
@@ -11,35 +11,37 @@ function Authentication() {
     setActive(active === "login" ? "signup" : "login");
   };
   return (
-    <div className="w-[100%]">
-      {active === "login" ? <Login /> : <Signup />}{" "}
-      <Typography
-        variant="small"
-        color="gray"
-        className="text-center font-normal"
-      >
-        {active === "login" ? (
-          <>
-            Not registered?{" "}
-            <button
-              onClick={handleChange}
-              className="font-medium text-gray-900"
-            >
-              Create an Account
-            </button>
-          </>
-        ) : (
-          <>
-            Have an account?{" "}
-            <button
-              onClick={handleChange}
-              className="font-medium text-gray-900"
-            >
-              Log in
-            </button>
-          </>
-        )}
-      </Typography>
+    <div className="w-full">
+      <div className="w-[100%]">
+        {active === "login" ? <Login /> : <Signup />}{" "}
+        <Typography
+          variant="small"
+          color="gray"
+          className="text-center font-normal"
+        >
+          {active === "login" ? (
+            <>
+              Not registered?{" "}
+              <button
+                onClick={handleChange}
+                className="font-medium text-gray-900"
+              >
+                Create an Account
+              </button>
+            </>
+          ) : (
+            <>
+              Have an account?{" "}
+              <button
+                onClick={handleChange}
+                className="font-medium text-gray-900"
+              >
+                Log in
+              </button>
+            </>
+          )}
+        </Typography>
+      </div>
     </div>
   );
 }
