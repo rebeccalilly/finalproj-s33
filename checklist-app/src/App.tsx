859d13e6dc03ab259305d78aae2e67713e055a3b
Jelizaveta Tsalaja <jelizaveta.tsalaja@gmail.com>
2024-05-23 16:57:47 -0400

    Added transition from Sign in, Sign out to other pages. Added Log out logic

--- checklist-app/src/App.tsx
+++ checklist-app/src/App.tsx
@@ -5,21 +5,43 @@ import Profile from "./Profile";
 import Authentication from "./authentication";
 import Checklistcard from "./Checklistcard";
 import { Button } from "@material-tailwind/react";
+import { getAuth, signOut } from "firebase/auth";
 
 function App() {
   const [whichPage, setWhichPage] = useState(1);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleSetCurrentUser = (user) => {
+    setCurrentUser(user);
+  };
+
+  const handleLogout = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        setCurrentUser(null);
+        console.log("logged out sucessfully");
+        setWhichPage(0);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <div className="h-lvh w-auto grid place-content-center">
       <div className="flex flex-row space-x-4">
-        {whichPage === 0 ? (
+        {currentUser === null ? (
           <>
-            <Authentication setWhichPage={setWhichPage} />
+            <Authentication
+              setWhichPage={setWhichPage}
+              setCurrentUser={handleSetCurrentUser}
+            />
           </>
         ) : whichPage === 1 ? (
           <>
             <Button onClick={() => setWhichPage(2)}>Go to Note card</Button>
-            <Button onClick={() => setWhichPage(0)}>Logout</Button>
+            <Button onClick={handleLogout}>Logout</Button>
 
             <br></br>
 
@@ -28,7 +50,7 @@ function App() {
         ) : (
           <>
             <Button onClick={() => setWhichPage(1)}>Go to Checklist</Button>
-            <Button onClick={() => setWhichPage(0)}>Logout</Button>
+            <Button onClick={handleLogout}>Logout</Button>
 
             <br></br>
 
@@ -41,3 +63,4 @@ function App() {
 }
 
 export default App;
+
